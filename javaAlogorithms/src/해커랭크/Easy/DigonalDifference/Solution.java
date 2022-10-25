6040ca14835b610bb23914926f2b5fa217da5dd0
hhiyeon <hhyyeon0214@gmail.com>
2022-10-25 17:25:12 +0900

    commit

---
+++ javaAlogorithms/src/해커랭크/Easy/DigonalDifference/Solution.java
@@ -0,0 +1,57 @@
+package 해커랭크.Easy.DigonalDifference;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int res1 = IntStream.range(0, arr.size())
+                .map(i -> arr.get(i).get(i))
+                .sum();
+        int res2 = IntStream.range(0, arr.size())
+                .map(i -> arr.get(i).get(arr.size()-1-i))
+                .sum();
+
+        return Math.abs(res1 - res2);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
